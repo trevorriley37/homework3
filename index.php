dd6929645ac101c8ba4a4edc952af908a1d18dec
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-09-21 14:15:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ echo "Connected successfully";
 $sql = "SELECT prefix,number,description FROM course";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows >= 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     echo "id: " . $row["prefix"]. " - Name: " . $row["number"]. " " . $row["description"]. "<br>";
