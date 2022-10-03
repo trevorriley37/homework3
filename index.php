e4f51da81fed00533978e671bd4cff91109b8340
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-03 15:13:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <table class="table">
+  <table class="table table-dark table-striped">
   <thead>
     <tr>
       <th scope="col">City ID</th>
