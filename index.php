bcfe52c509297d7802b8d570a6c4bdf3251b5e5d
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-03 15:06:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,11 +24,23 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["Abbreviation"]?></td>
-    <td><?=$row["fullname"]?></td>
-    
-  </tr>
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>["Abbreviation"]</td>
+      <td>["fullname"]</td>
+    </tr>
+  </tbody>
+</table>
 <?php
   }
 } else {
