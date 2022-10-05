bd0a135fad2600894f9928f3bc05c3beabc3ae76
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 19:55:57 -0500

    Update basketball.php

--- basketball.php
+++ basketball.php
@@ -37,10 +37,10 @@ if ($result->num_rows > 0) {
   </thead>
   <tbody>
     <tr>
-     <align="right">
      
     <th scope="row"><?=$row["city_ID"]?></th> 
      <td> <?=$row["Team_Name"]?></td>
+     <align="right">
     </tr>
   </tbody>
 </table>
