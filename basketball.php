069e8a221cf477d466c85673f93ae207a149230f
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 19:44:40 -0500

    Update basketball.php

--- basketball.php
+++ basketball.php
@@ -1,3 +1,50 @@
 <?php require_once("header.php"); ?>
 
+$sql = "SELECT city_ID,Team_Name from basketball order by city_ID";
+$result = $conn->query($sql);
+
+ ?> <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">City ID</th>
+      <th scope="col"></th>
+      <th scope="col"> Team Name</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+    
+    </tr>
+  </tbody>
+</table>
+<?php
+
+
+
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ 
+     ?> <table class="table table-dark table-striped"
+               align="center">
+  <thead>
+    
+  </thead>
+  <tbody>
+    <tr>
+     
+    <th scope="row"><?=$row["city_ID"]?></th>
+      
+      <td><?=$row["Team_Name"]?></td>
+    </tr>
+  </tbody>
+</table>
+<?php
+  }
+} else {
+  echo "0 results";
+}
 
