97ba3dffb2166e34fbdf9d1821c93a3d4b1c92ac
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 19:40:49 -0500

    Update header.php

--- header.php
+++ header.php
@@ -32,14 +32,14 @@ echo "Connected successfully";
 
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Cities</a>
+    <a class="navbar-brand" href="index.php">Cities</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Basketball</a>
+          <a class="nav-link active" aria-current="page" href="basketball.php">Basketball</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Football</a>
