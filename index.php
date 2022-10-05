5edf8ff59a71e6a63a1507f62a6c89eceeabef36
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 21:51:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,21 @@
 <?php require_once("header.php"); ?>
 
-<?php
 
-
-
-$sql = "SELECT city_ID,Abbreviation, fullname from City order by city_ID";
-$result = $conn->query($sql);
-
- ?> <table class="table table-dark table-striped">
+<table class="table table-dark table-striped">
   <thead>
     <tr>
       <th scope="col">City ID</th>
       <th scope="col"></th>
       <th scope="col">Name</th>
-      
     </tr>
   </thead>
   <tbody>
-    <tr>
-     
-    
-    </tr>
-  </tbody>
-</table>
 <?php
+
+$sql = "SELECT city_ID,Abbreviation, fullname from City order by city_ID";
+$result = $conn->query($sql);
+
+
 
 
 
@@ -32,25 +24,24 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
  
-     ?> <table class="table table-dark table-striped"
-               align="center">
-  <thead>
-    
-  </thead>
-  <tbody>
+    ?>
     <tr>
      
-    <th scope="row"><?=$row["city_ID"]?></th>
-      <td><?=$row["Abbreviation"]?></td>
-      <td><?=$row["fullname"]?></td>
+   <td><?=$row["city_ID"]?></td>
+    <td><?=$row["Abbreviation"]?></td>
+   <td><?=$row["fullname"]?></td>
     </tr>
-  </tbody>
-</table>
+ 
 <?php
   }
 } else {
   echo "0 results";
 }
+   
+  ?> 
+   </tbody>
+</table>
+</html>
 
 
 
