2a7f9c5e28b20d081844ccfcc57fe1c173999912
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-05 12:24:26 -0500

    Update posthandle.php

--- posthandle.php
+++ posthandle.php
@@ -10,9 +10,7 @@
 
   
   
-<?php
-  $sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID where fullname=" .  $_POST['fname'];
-$result = $conn->query($sql);
+<?
   
   
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -27,6 +25,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
 }
 
+  
+  <?php
+  $sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID where fullname=" .  $name;
+$result = $conn->query($sql);
+  
+  
   if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
