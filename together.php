c60fb876daf3eb12f7ca5b171588303cb34b1417
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 20:27:46 -0500

    Update together.php

--- together.php
+++ together.php
@@ -1,1 +1,5 @@
 <?php require_once("header.php"); ?>
+
+
+
+SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,ba.Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID
