1ce5e9442c87efb56e25c16712fec5b66055f579
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 21:31:30 -0500

    Update together.php

--- together.php
+++ together.php
@@ -1,51 +1,51 @@
 <?php require_once("header.php"); ?>
 
-<?
-$sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,ba.Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID";
-$result = $conn->query($sql);
 
- ?> <table class="table table-dark table-striped">
+
+<table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">City ID</th>
-      <th scope="col">Abbreviation</th>
-      <th scope="col">Name</th>
-      <th scope="col">Baseball Team</th>
-     <th scope="col">Football Team</th>
-     <th scope="col">Basketball Team</th>
+      <th>City_ID</th>
+      <th>Abbreviation</th>
+     <th>Name</th>
+     <th>Baseball Team</th>
+     <th>Football Team</th>
+     <th>Basketball Team</th>
     </tr>
   </thead>
-<tbody>
-
-<?php
- 
-
-
+  <tbody>
+    <?php
+   
+   
+$sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,ba.Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID";
+$result = $conn->query($sql);
+   
+   
+   
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
- 
 ?>
- 
- <tbody>
- 
-    <tr> <table class="table table-dark table-striped">
-      <th scope="row"><?=$row["city_ID"]?></th>
-      <td><?=$row["Abbreviation"]?></td>
-      <td><?=$row["fullname"]?></td>    
-      <td><?=$row["Baseball_name"]?></td>
-      <td><?=$row["football_name"]?></td>
-      <td><?=$row["ba.Team_Name"]?></td>
-    </tr>
-    </tbody>
-</table>
-<?php
-
-  }
+  <tr>
+    <td><?=$row["c.city_ID"]?></td>
+    <td><?=$row["Abbreviation"]?></td>
+   <td><?=$row["fullname"]?></td>
     
+  </tr>
+<?php
+  }
 } else {
   echo "0 results";
 }
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+ 
 
 
 
