694a85603e0ac4be76e57558aac24c766e0e0b6d
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-05 12:40:43 -0500

    Update posthandle.php

--- posthandle.php
+++ posthandle.php
@@ -1,21 +1,26 @@
 <?php require_once("header.php"); ?>
 
 
-form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
   Name: <input type="text" name="fname">
   <input type="submit">
 </form>
 
 <?php
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
     $name = $_POST['fname'];
-    
-?>
-      
-
-  <?php
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        <?php
   $sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID where fullname=" .  $name;
 $result = $conn->query($sql);
+    }
+}
+?>
+
+  
   
   
   if ($result->num_rows > 0) {
