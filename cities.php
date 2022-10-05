424898c548f3a03c75c35873f02eee4016f53e8d
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 22:22:55 -0500

    Update cities.php

--- cities.php
+++ cities.php
@@ -16,3 +16,27 @@
     
     $sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID where c.city_ID= . $iid order by c.city_ID";
 $result = $conn->query($sql);
+
+    
+ if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["fullname"]?></td>
+    <td><?=$row["Team_Name"]?></td>
+    <td><?=$row["football_name"]?></td>
+    <td><?=$row["Baseball_name"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
