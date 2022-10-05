3de9187dfc6f53b9efb110fbf134aa23390b1786
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-05 12:31:30 -0500

    Update posthandle.php

--- posthandle.php
+++ posthandle.php
@@ -1,34 +1,6 @@
 <?php require_once("header.php"); ?>
 
-<html>
-<body>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
-</form>
-
-  
-  
-<?php
-  
-  
-form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  <input type="submit">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_POST['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
-    }
-}
-?>
       
 
   <?php
