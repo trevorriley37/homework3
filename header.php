b1d76516d7b2af143958e09962a58e868bb9d7d0
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 19:41:28 -0500

    Update header.php

--- header.php
+++ header.php
@@ -12,21 +12,6 @@
   </body>
 </html>
 
-<?php
-$servername = "localhost";
-$username = "trileyou_rile0056";
-$password = "T833481t!";
-$dbname = "trileyou_database1";
-  
-
-// Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 
 
 
