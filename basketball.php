184380a17ed98fa0db23143025e46dccd9f7da11
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 21:53:40 -0500

    Update basketball.php

--- basketball.php
+++ basketball.php
@@ -1,27 +1,21 @@
 <?php require_once("header.php"); ?>
 
-<?
 
-$sql = "SELECT city_ID,Team_Name from Basketball order by city_ID";
-$result = $conn->query($sql);
-
- ?> <table class="table table-dark table-striped">
+<table class="table table-dark table-striped">
   <thead>
     <tr>
       <th scope="col">City ID</th>
       <th scope="col"></th>
-      <th scope="col"> Team Name</th>
-      
+      <th scope="col">Basketball Team Name</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-     
-    
-    </tr>
-  </tbody>
-</table>
 <?php
+
+$sql = "SELECT city_ID,Team_Name from Basketball order by city_ID";
+$result = $conn->query($sql);
+
+
 
 
 
@@ -30,23 +24,21 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
  
-     ?> <table class="table table-dark table-striped" align="center">
-               
-  <thead>
-    
-  </thead>
-  <tbody>
+    ?>
     <tr>
      
-    <th scope="row"><?=$row["city_ID"]?></th> 
-     <td> <?=$row["Team_Name"]?></td>
-     <align="right">
+   <td><?=$row["city_ID"]?></td>
+    <td><?=$row["Team_Name"]?></td>
     </tr>
-  </tbody>
-</table>
+ 
 <?php
   }
 } else {
   echo "0 results";
 }
+   
+  ?> 
+   </tbody>
+</table>
+</html>
 
