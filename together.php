118fa7933a2b3d80a5ab780d76bf0927dae165ea
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-04 20:28:36 -0500

    Update together.php

--- together.php
+++ together.php
@@ -1,5 +1,53 @@
 <?php require_once("header.php"); ?>
 
 
+$sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,ba.Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID";
+$result = $conn->query($sql);
 
-SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,ba.Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID order by c.city_ID
+ ?> <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">City ID</th>
+      <th scope="col"></th>
+      <th scope="col">Name</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+    
+    </tr>
+  </tbody>
+</table>
+<?php
+
+
+
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ 
+     ?> <table class="table table-dark table-striped"
+               align="center">
+  <thead>
+    
+  </thead>
+  <tbody>
+    <tr>
+     
+    <th scope="row"><?=$row["city_ID"]?></th>
+      <td><?=$row["Abbreviation"]?></td>
+      <td><?=$row["fullname"]?></td>
+    </tr>
+  </tbody>
+</table>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+
+
+
