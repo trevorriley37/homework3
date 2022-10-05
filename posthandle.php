af0b2896ca9bf2e6b2e1b04071cd42a214abfdea
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-05 12:28:19 -0500

    Update posthandle.php

--- posthandle.php
+++ posthandle.php
@@ -13,19 +13,15 @@
 <?
   
   
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $name = $_POST['fname'];
     if (empty($name)) {
         echo "Name is empty";
     } else {
-        
-    
-  
-  
-}
+      
 
-  
   <?php
   $sql = "SELECT c.city_ID,Abbreviation ,fullname, Baseball_name, football_name,Team_Name FROM Baseball b join Football f on b.city_ID =f.city_ID join City c on f.city_ID =c.city_ID join Basketball ba on c.city_ID = ba.city_ID where fullname=" .  $name;
 $result = $conn->query($sql);
