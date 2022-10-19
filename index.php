ca1ae6b6350d213474746dea74527a5a8d976d8f
trevorriley37 <97976254+trevorriley37@users.noreply.github.com>
2022-10-19 13:37:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ if ($result->num_rows > 0) {
      
    <td><?=$row["city_ID"]?></td>
     <td><?=$row["Abbreviation"]?></td>
-    <td><a name="id" value=<?=$row["fullname"]?> "href="cities.php" ><?=$row["fullname"]?></a></td>
+    <td><a name="id" value=<?=$row["city_ID"]?> "href="cities.php" ><?=$row["fullname"]?></a></td>
     </tr>
  
 <?php
